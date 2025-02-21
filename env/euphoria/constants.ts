cbe55780030c18f1e2466f8d59b5e8ab73bc218c
conan321 <minhduc0401@gmail.com>
2025-02-21 18:05:02 +0700

    Update contract address usdt

--- env/euphoria/constants.ts
+++ env/euphoria/constants.ts
@@ -3,4 +3,4 @@ export const WETH_ADDRESS = '0x220c08739c70a3e33b8b7e90f6076751415cc6c2' // weth
 export const USDC_WETH_PAIR = '' // usdc is token0 - lowercase
 export const DAI_WETH_PAIR = ''
 export const USDT_WETH_PAIR = '0xc2343025bb90398300a82f688f82f92ce22b6864' // usdt is token1 - lowercase
-export const WHITELIST = ['0x220c08739c70a3e33b8b7e90f6076751415cc6c2', '0xc2343025bb90398300a82f688f82f92ce22b6864'] // WETH & USDC Contracts, split by comma, lowsercase
+export const WHITELIST = ['0x220c08739c70a3e33b8b7e90f6076751415cc6c2', '0x59a1fccf28ba3dad00e457e45349667ada1fa7c3'] // WETH & USDT Contracts, split by comma, lowsercase
