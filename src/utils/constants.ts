ce1fed73f842fd25e628386d075799082ebe9b39
conan321 <minhduc0401@gmail.com>
2025-02-21 17:57:18 +0700

    Update usdt contract eupho

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
-export const FACTORY_ADDRESS = '0x80bfECE542B8165C6ef0e5D0d859bd08f2c09059' // factory address - normal
+export const FACTORY_ADDRESS = '0x6685B976504631b6C189d521586C02c7055371e6' // factory address - normal
 export const WETH_ADDRESS = '0x220c08739c70a3e33b8b7e90f6076751415cc6c2' // weth address - lowercase
 export const USDC_WETH_PAIR = '' // usdc is token0 - lowercase
 export const DAI_WETH_PAIR = ''
-export const USDT_WETH_PAIR = '' // usdt is token1 - lowercase
-export const WHITELIST = ['0x220c08739c70a3e33b8b7e90f6076751415cc6c2'] // WETH & USDC Contracts, split by comma, lowsercase
+export const USDT_WETH_PAIR = '0xc2343025bb90398300a82f688f82f92ce22b6864' // usdt is token1 - lowercase
+export const WHITELIST = ['0x220c08739c70a3e33b8b7e90f6076751415cc6c2', '0xc2343025bb90398300a82f688f82f92ce22b6864'] // WETH & USDC Contracts, split by comma, lowsercase
